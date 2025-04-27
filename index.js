947192abda3d5948efc45617cc95917a9f4eb07c
atiksh12356 <atikshsharma048@gmail.com>
2025-04-27 22:08:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
-var port = data["port"];
-var username = data["name"]
+var host = data["LapataSMP-Zewp.aternos.me"];
+var port = data["56280"];
+var username = data["noob"]
 var bot = mineflayer.createBot({
-  host: host,
-  port: port,
-  username: username
+  host: LapataSMP-Zewp.aternos.me,
+  port: 56280,
+  username: noob
 });
 function getRandomArbitrary(min, max) {
        return Math.random() * (max - min) + min;
